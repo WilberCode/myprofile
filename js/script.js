e3f44bde3f4a3d6fcd6222fe177ceaf2ec886689
Wilber Pariona <parionahuaraca@gmail.com>
2018-03-02 18:40:14 -0500

    Fixed Mistakes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //Typing Effect
 let i = 0;
-let txt ='Técnico egresado de Telemática: Informática y Sistemas con conocimientos en desarrollo web, diseño web, programacion basica, aplicaciones moviles basicas y idioma ingles intermedio. Dispuesto a laborar como practicante con muchas ganas de aprender y superarse como profesional';
+let txt ='Técnico egresado de Telemática: Informática y Sistemas con conocimientos en desarrollo web, diseño web, programacion basica, aplicaciones moviles basicas y idioma ingles intermedio. Dispuesto a laborar como practicante con muchas ganas de aprender y superarse como profesional.';
 let speed = 50;
 function typingEffect(){
     if(i < txt.length){
