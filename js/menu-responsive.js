c30a2bb2ccb02d6b58713cff2d60aa07e419e533
Wilber Pariona <parionahuaraca@gmail.com>
2018-03-09 19:55:45 -0500

    Se agrego responsive al menu en la section nav

---
+++ js/menu-responsive.js
@@ -0,0 +1,24 @@
+let navContainerLogo   = document.getElementById('nav-container-logo');
+let menuIcon = document.createElement('div');
+menuIcon.className = 'menuIcon';
+navContainerLogo.appendChild(menuIcon); 
+let menu = document.getElementById('menu'); 
+
+menuIcon.addEventListener("click",function(event){  
+    if(menu.style.display === 'block'){ 
+       menu.style.display = 'none';
+    }else{
+        menu.style.display = 'block';
+    } 
+
+});
+let menuMatch =(x)=>{   
+    if(x.matches){ 
+        menu.style.display ='none';  
+    }else{  
+        menu.style.display ='flex';  
+    } 
+} 
+let x = window.matchMedia("(max-width: 1023px)")
+menuMatch(x)
+x.addListener(menuMatch);  
