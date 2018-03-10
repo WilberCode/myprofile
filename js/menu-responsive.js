afc60777f0832e9ffc01f24c585a87c708ec6837
Wilber Pariona <parionahuaraca@gmail.com>
2018-03-09 23:25:34 -0500

    Fixed mistakes

--- js/menu-responsive.js
+++ js/menu-responsive.js
@@ -1,17 +1,18 @@
-let navContainerLogo   = document.getElementById('nav-container-logo');
+/*Codigo Js para menu responsive*/
+let navContainerLogo  = document.getElementById('nav-container-logo');
 let menuIcon = document.createElement('div');
 menuIcon.className = 'menuIcon';
 navContainerLogo.appendChild(menuIcon); 
 let menu = document.getElementById('menu'); 
 
 menuIcon.addEventListener("click",function(event){  
-    if(menu.style.display === 'block'){ 
-       menu.style.display = 'none';
+    if( menu.style.display ==='block'){ 
+        menu.style.display='none';     
     }else{
-        menu.style.display = 'block';
-    } 
-
+        menu.style.display='block';   
+    }  
 });
+/* Match Media for menu responsive  */
 let menuMatch =(x)=>{   
     if(x.matches){ 
         menu.style.display ='none';  
